36bdc2f71de751ce7614cb95e0dec5dbfb39b50d
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-11 11:19:43 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './index.css';
+import Navbar from "./Navbar";
+import Banner1 from "./Banner1"
+
+function App() {
+  return (
+    <div>
+    <div className="banner">
+  <Navbar/>
+  <Banner1/>
+  </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Banner1.js
@@ -0,0 +1,15 @@
+
+
+function Banner1(){
+    return(
+        <div className=" banner-text">
+
+<div>
+<h1 className="banner-text-header">A Classical Education for the Future</h1>
+<p className=" banner-text-paragraph">  We prepare you to engage in the world that is to and help bring about a world that ought to be</p>
+<button className=" buttons banner1-button"> Get started</button>
+        </div>
+</div>
+    );
+}
+export default Banner1;
