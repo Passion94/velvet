aea287f5b410da6a39ec8dc97862ea4d61ab981f
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-12 12:21:15 -0800

    Testing commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import './index.css';
 import Navbar from "./Navbar";
 import Banner1 from "./Banner1"
+import Livechatsection from "./Livechatsection";
 
 function App() {
   return (
-    <div>
+    <div className='maincontainer'>
     <div className="banner">
   <Navbar/>
   <Banner1/>
   </div>
+  <Livechatsection/>
     </div>
   );
 }
--- src/Banner1.js
+++ src/Banner1.js
@@ -4,11 +4,11 @@ function Banner1(){
     return(
         <div className=" banner-text">
 
-<div>
-<h1 className="banner-text-header">A Classical Education for the Future</h1>
-<p className=" banner-text-paragraph">  We prepare you to engage in the world that is to and help bring about a world that ought to be</p>
+
+<h1 className="banner-text-header">A Classical Education <br/>for the Future</h1>
+<p className=" banner-text-paragraph">  We prepare you to engage in the world<br className="smbreaks"/> that is to and help bring<br className="lgbreaks"/>about a world<br className="smbreaks"/> that ought to be</p>
 <button className=" buttons banner1-button"> Get started</button>
-        </div>
+        
 </div>
     );
 }
