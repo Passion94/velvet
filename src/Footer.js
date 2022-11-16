9d42060f1c62eb6ba0ed977f6e529bd6e6ab8cf0
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-16 09:58:16 -0800

    Yes

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ function Footer(){
     return (
         <footer>
         <div className="footer">
-        <h5> Subscribe to get latest Updates</h5>
+        <h5 className="sub-update"> Subscribe to get latest Updates</h5>
 <div className="form-sub-section">
       <input type="text" placeholder="Your Email address" name="Subscribe"/>
       <button className="smsub-button">Subscribe</button>
@@ -11,21 +11,24 @@ function Footer(){
 
 
 <div className="footer-sub-section">
-<p><span> Address:</span>38 opebi Road, Ikeja, Lagos State, Nigeia.</p>
-<p> Phone:+2349022396389</p>
-<p>Email:contact@contentionary.com</p>
+
+<div className="footer-sub-section-one">
+<p><span className="address"> Address:</span><br/>38 opebi Road, Ikeja, Lagos <br/>State, Nigeia.</p>
+<p><span className="address">Phone:</span><br/> +2349022396389</p>
+<p><span className="address">Email:</span><br/>contact@contentionary.com</p>
 </div>
+
+
+
 <div>
-
-
-
 <p> Company</p>
 <a className="sub-link" href="#"> About</a>
 <a className="sub-link" href="#">Features</a>
 <a className="sub-link" href="#">Pricing</a>
-
-
 </div>
+</div>
+
+
 
         </footer>
     );
