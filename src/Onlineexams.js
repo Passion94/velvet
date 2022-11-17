d882619d7fc003dbb1dbba911cec3021469d3901
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-17 09:36:09 -0800

    ALmost complete

--- src/Onlineexams.js
+++ src/Onlineexams.js
@@ -10,15 +10,15 @@ function Onlineexams(){
     
         <div  className="experts-image-and-text-container">
             <div className="experts-image-container">
-            <img id="experts-woman-image" src="images/Experts/image1.png" />
+            <img id="experts-woman-image" src="images//online.svg" />
             <img className="expert-block-image"src="images/Experts/image3.svg"/>
             </div>
             
         <div className="expert-textand-button">
             
-            <h1> We are <span className="student"> Experts</span> Learning institution</h1>
-            <p className="expert-textand-button-para"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit elit, bibendum volutpat platea mi, adipiscing eget magna. </p>
-            <button className="  expert-button"> Get started</button>
+            <h1> Our online <span className="student"> examination</span> <br/>is top-notch</h1>
+            <p className="expert-textand-button-para"> Prepare your self for greater challenges with our online examination. </p>
+            <button className="  expert-button"> Take a tour</button>
         
         </div>
     </div>
