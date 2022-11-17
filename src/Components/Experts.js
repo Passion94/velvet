fa487b866936f18f4c2075217bfad7ce9511e593
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-17 08:03:53 -0800

    Almost done

--- src/Components/Experts.js
+++ src/Components/Experts.js
@@ -2,18 +2,23 @@
 function Experts(){
     return(
     <div className='expert-container'>
+
+
+<div className="dotted-circle"></div>
+<div className="close-circle"></div>
+
     
         <div  className="experts-image-and-text-container">
             <div className="experts-image-container">
             <img id="experts-woman-image" src="images/Experts/image1.png" />
-            <img src="images/Experts/image3.svg"/>
+            <img className="expert-block-image"src="images/Experts/image3.svg"/>
             </div>
             
         <div className="expert-textand-button">
             
-            <h1> We are Experts Learning institution</h1>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit elit, bibendum volutpat platea mi, adipiscing eget magna.</p>
-            <button className=" buttons expert-button"> Get started</button>
+            <h1> We are <span className="student"> Experts</span> Learning institution</h1>
+            <p className="expert-textand-button-para"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit elit, bibendum volutpat platea mi, adipiscing eget magna. </p>
+            <button className="  expert-button"> Get started</button>
         
         </div>
     </div>
