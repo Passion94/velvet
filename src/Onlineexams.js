dafc8d2df77b965c5fb2f92c25847820d4d28a6e
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-12-02 03:23:39 -0800

    tryialkshbd

--- src/Onlineexams.js
+++ src/Onlineexams.js
@@ -7,14 +7,8 @@ function Onlineexams(){
 
 
     
-        <div  className="experts-image-and-text-container">
-        <div className="expert-textand-button online-arrange1">
-            
-            <p> Our online <br/><span className="student"> examination</span> is<br/> top-notch</p>
-            <p className="expert-textand-button-para"> Prepare your self for greater challenges<br/> with our online examination. </p>
-            <button className="buttons "> Take a test</button>
-        
-        </div>
+        <div  className="experts-image-and-text-container online-container">
+
         <div className="online-arrange">
         <div className="dotted-circle"></div>
         <div className="close-circle"></div>
@@ -22,6 +16,14 @@ function Onlineexams(){
             <img id="experts-woman-image" src="images//online.svg" />
             <img className="expert-block-image online-arrange3"src="images/Experts/onlineblock.svg"/>
             </div>
+        <div className="expert-textand-button online-arrange1">
+            
+            <p> Our online <br/><span className="student"> examination</span> is<br/> top-notch</p>
+            <p className="expert-textand-button-para"> Prepare your self for greater challenges<br/> with our online examination. </p>
+            <button className="buttons "> Take a test</button>
+        
+        </div>
+       
             </div>
         
     </div>
